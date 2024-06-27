bc6eb098c9cf03fac97a99b30cae9f99c60850e9
denis <denis.vespasiano@live.com>
2024-06-27 16:12:09 -0300

    Startinc CSS

---
+++ src/support-contact/SupportContact.css
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SupportContact = ({ name, email, phone }) => (
+  <div className="support-contact">
+    <h2>Your Feefo Support Contact</h2>
+    <div className="contact-details">
+      <span aria-label="Support Initial" className="contact-initial">S</span>
+      <div>
+        <p>{name}</p>
+        <p><a href={`mailto:${email}`}>{email}</a></p>
+        <p>{phone}</p>
+      </div>
+    </div>
+  </div>
+);
+
+SupportContact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
+
+export default SupportContact;
