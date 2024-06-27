e8b420f7ec006d63b561afcabff789be6c35b4e7
denis <denis.vespasiano@live.com>
2024-06-27 19:47:58 -0300

    finishing second content

--- src/sales-info/SalesInfo.jsx
+++ src/sales-info/SalesInfo.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 import './SalesInfo.css';
-import '../app.css';
 
 const SalesInfo = ({ uploads, linesAdded }) => (
   <div className="sales-info">
+    <div className="sales-info-title">
+    <div className="sales-info-title-sales">
+    <FontAwesomeIcon icon={faUpload} />
     <h3>Sales</h3>
-    <p>You had {uploads} uploads and {linesAdded} lines added.</p>
+    </div>
+    <div className="sales-info-info">
+    <FontAwesomeIcon icon={faInfoCircle} />
+    </div>
+    </div>
+    <p>You had <strong> {uploads} uploads</strong> and <strong> {linesAdded}</strong> lines added.</p>
   </div>
 );
 
